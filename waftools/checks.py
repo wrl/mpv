a5768692f563e1e3e17df9b977d94b85a3cfd229
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2013-10-02 19:05:50 +0200

    waf: remove iteration hack in check_pkg_config

--- waftools/checks.py
+++ waftools/checks.py
@@ -1,5 +1,8 @@
 any_version = None
 
+def even(n):
+    return n % 2 == 0
+
 def check_cc(**kw):
     def fn(ctx, dependency_identifier):
         argsl    = list(args)
@@ -15,7 +18,7 @@ def check_cc(**kw):
 def check_pkg_config(*args):
     def fn(ctx, dependency_identifier):
         argsl    = list(args)
-        packages = [i for i in args if argsl.index(i) % 2 == 0]
+        packages = [el for (i, el) in enumerate(args) if even(i)]
         sargs    = [i for i in args if i] # remove None
         return ctx.check_cfg(package=" ".join(packages),
                              args=sargs + ["--libs", "--cflags"],
