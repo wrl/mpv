4a09a802247e242088741a43c3d3cbd4035553e7
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2013-10-03 21:32:31 +0200

    waf: checks, make them META

--- waftools/checks.py
+++ waftools/checks.py
@@ -3,37 +3,39 @@ any_version = None
 def even(n):
     return n % 2 == 0
 
-def default_check_cc_options(dependency_identifier, kw=None):
-    kw = kw or {}
-    kw['uselib_store'] = dependency_identifier
-    kw['mandatory'] = False
-    return kw
+def merge_options(dependency_identifier, *args):
+    initial_values = {
+        'uselib_store': dependency_identifier,
+        'mandatory': False }
+
+    def merge_dicts(r, n):
+        return n and dict(r.items() + n.items()) or r
+
+    return reduce(merge_dicts, args, initial_values)
 
 def check_libs(libs, function):
     libs = [""] + libs
     def fn(ctx, dependency_identifier):
         for lib in libs:
-            if function(ctx, dependency_identifier, {'lib': lib}):
+            if function(ctx, dependency_identifier, lib=lib):
                 return True
         return False
     return fn
 
 def check_statement(header, statement):
-    def fn(ctx, dependency_identifier, cc_defaults=None):
-        kw = default_check_cc_options(dependency_identifier, cc_defaults)
-        kw['fragment'] = """
+    def fn(ctx, dependency_identifier, **kw):
+        fragment = """
             #include <{0}>
             int main(int argc, char **argv)
             {{ {1}; return 0; }} """.format(header, statement)
-        return ctx.check_cc(**kw)
+        opts = merge_options(dependency_identifier, {'fragment':fragment}, kw)
+        return ctx.check_cc(**opts)
     return fn
 
-def check_cc(**kw):
-    def fn(ctx, dependency_identifier, cc_defaults=None):
-        if cc_defaults:
-            kw.update(cc_defaults)
-        default_check_cc_options(dependency_identifier, kw)
-        return ctx.check_cc(**kw)
+def check_cc(**kw_ext):
+    def fn(ctx, dependency_identifier, **kw):
+        options = merge_options(dependency_identifier, kw_ext, kw)
+        return ctx.check_cc(**options)
     return fn
 
 
